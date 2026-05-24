7e460b93725d85a7bd1d0de45edb41ee496e0f9a
rezadehhaghi <rezadehhaghi.fi@gmail.com>
2026-05-24 15:46:33 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener(
+  "install",
+  event => {
+
+    console.log(
+      "Service Worker Installed"
+    );
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+  }
+);
